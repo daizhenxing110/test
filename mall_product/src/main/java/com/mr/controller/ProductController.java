24c5dac6630539ef2388f1ab6d543b5b875868f4
daizhenxing <411591397@qq.com>
2019-12-17 20:05:33 +0800

    idea整合github测试

---
+++ mall_product/src/main/java/com/mr/controller/ProductController.java
@@ -0,0 +1,19 @@
+package com.mr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("product")
+public class ProductController {
+    @GetMapping("we")
+    public void testDome(){
+
+
+
+        System.out.println(Test.test());
+    }
+
+
+}
